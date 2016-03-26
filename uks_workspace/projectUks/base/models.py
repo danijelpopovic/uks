195bb48e453bf23f4b7efd606052bea88ea6c289
danijelpopovic <d.popovic89@gmail.com>
2016-03-26 16:28:39 +0100

    register user

--- uks_workspace/projectUks/base/models.py
+++ uks_workspace/projectUks/base/models.py
@@ -1,3 +1,12 @@
-from django.db import models
-
-# Create your models here.
+# import UserProfile as UserProfile
+# from django.contrib.auth.models import User
+# from django.forms import ModelForm
+#
+# class UserForm(ModelForm):
+#     class Meta:
+#         model = User
+#
+# class UserProfileForm(ModelForm):
+#     class Meta:
+#         model = UserProfile
+#         exclude = ['user']
